37824ed4921eaf347865e0cd35edb6bd81d58a14
Alfonso Saavedra "Son Link <sonlink.dourden@gmail.com>
2023-08-22 20:00:41 +0200

    Fixed 2 files indention

--- src/ene1.js
+++ src/ene1.js
@@ -2,16 +2,16 @@ import { Enemy } from './enemy'
 import Ene1Sprite from './assets/ene1.png'
 
 class Ene1 extends Enemy {
-	constructor(x, y) {
-		const options = {
-			x: x,
-			y: y,
-			sprite: Ene1Sprite,
+  constructor(x, y) {
+    const options = {
+      x: x,
+      y: y,
+      sprite: Ene1Sprite,
       life: 100
-		}
-		super(options)
-		this.options = options
-	}
+    }
+    super(options)
+    this.options = options
+  }
 }
 
 export { Ene1 }
--- src/ene2.js
+++ src/ene2.js
@@ -2,16 +2,16 @@ import { Enemy } from './enemy'
 import sprite from './assets/ene2.png'
 
 class Ene2 extends Enemy {
-	constructor(x, y) {
-		const options = {
-			x: x,
-			y: y,
-			sprite: sprite,
+  constructor(x, y) {
+    const options = {
+      x: x,
+      y: y,
+      sprite: sprite,
       life: 120
-		}
-		super(options)
-		this.options = options
-	}
+    }
+    super(options)
+    this.options = options
+  }
 }
 
 export { Ene2 }
